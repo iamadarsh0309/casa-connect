cebe155d03be5b5ba08ecab9f7216965eea09d36
Adarsh Pandey <adarshpandeyy00@gmail.com>
2025-06-06 23:25:47 +0530

    add: Created entity package with User, Property, and Booking classes

---
+++ src/main/java/com/example/airbnb/AirBnB/Clone/Entity/Booking.java
@@ -0,0 +1,26 @@
+package com.example.airbnb.AirBnB.Clone.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
---
+++ src/main/java/com/example/airbnb/AirBnB/Clone/Entity/Property.java
@@ -0,0 +1,28 @@
+package com.example.airbnb.AirBnB.Clone.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private Double pricePerNight;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+}
